8e70e557be1fbaf1897819d16650c99de191c580
Gumichan01 <luxon.jean.pierre@gmail.com>
2025-01-03 16:04:13 +0100

    Add basic code

---
+++ src/main/java/com/example/mugakusci/MugakusciApplication.java
@@ -0,0 +1,13 @@
+package com.example.mugakusci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugakusciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugakusciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mugakusci/MugakusciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mugakusci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugakusciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
