0da99119297153aa8e2fb94775de63d5bd7f3280
Gumichan01 <luxon.jean.pierre@gmail.com>
2025-01-03 17:08:56 +0100

    Reformat the main class

--- src/main/java/com/example/mugakusci/MugakusciApplication.java
+++ src/main/java/com/example/mugakusci/MugakusciApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MugakusciApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MugakusciApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MugakusciApplication.class, args);
+    }
 
 }
